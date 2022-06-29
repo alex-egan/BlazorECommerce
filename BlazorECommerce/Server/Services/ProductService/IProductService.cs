bf4f95fff9c3950a1069e8156986cefcb8560964
alex.egan1313@icloud.com <alex.egan1313@icloud.com>
2022-06-29 16:46:34 -0700

    Add Search Component to MainLayout

--- BlazorECommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorECommerce/Server/Services/ProductService/IProductService.cs
@@ -2,8 +2,10 @@
 {
     public interface IProductService
     {
-        public Task<ServiceResponse<List<Product>>> GetProductsAsync();
-        public Task<ServiceResponse<Product>> GetProductAsync(int productId);
-        public Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
+        Task<ServiceResponse<List<Product>>> GetProductsAsync();
+        Task<ServiceResponse<Product>> GetProductAsync(int productId);
+        Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
+        Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
+        Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText);
     }
 }
